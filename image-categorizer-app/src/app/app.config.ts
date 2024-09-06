dda0b4a9bff99f29e909ebc95575fa6ca3c3f6bf
Kevin Wüstner <kevin.wuestner@xappido.com>
2024-09-06 14:58:56 +0200

    feat(image-upload): add upload form

--- image-categorizer-app/src/app/app.config.ts
+++ image-categorizer-app/src/app/app.config.ts
@@ -1,6 +1,7 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
+import { provideHttpClient } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { Amplify } from 'aws-amplify';
 import awsConfig from '../aws-exports';
@@ -13,5 +14,6 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimationsAsync(),
+    provideHttpClient(),
   ],
 };
--- image-categorizer-app/src/app/image-upload/image-upload.component.ts
+++ image-categorizer-app/src/app/image-upload/image-upload.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-image-upload',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatButtonModule, MatIconModule],
   templateUrl: './image-upload.component.html',
-  styleUrl: './image-upload.component.scss'
+  styleUrl: './image-upload.component.scss',
 })
 export class ImageUploadComponent {
+  private _httpClient = inject(HttpClient);
 
+  onFileSelected(event: any, inputFile: File | null) {
+    const file: File = inputFile || event.target.files[0];
+
+    if (file) {
+      const formData = new FormData();
+      formData.append('file', file);
+    }
+  }
+
+  handleDragOver(event: DragEvent) {
+    event.preventDefault();
+    event.stopPropagation();
+  }
+
+  handleDrop(event: DragEvent) {
+    event.preventDefault();
+    if (event.dataTransfer) {
+      const file: File = event.dataTransfer.files[0];
+      this.onFileSelected(event, event.dataTransfer.files[0]);
+    }
+  }
 }
