3632122cf40b7dc3b3249c47c222598c4e802f2f
Kevin Wüstner <kevin.wuestner@xappido.com>
2024-09-06 13:26:52 +0200

    feat: update home page

--- image-categorizer-app/src/app/app.component.ts
+++ image-categorizer-app/src/app/app.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterOutlet } from '@angular/router';
 import { AmplifyAuthenticatorModule } from '@aws-amplify/ui-angular';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AmplifyAuthenticatorModule],
+  imports: [
+    RouterOutlet,
+    AmplifyAuthenticatorModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatListModule,
+    MatIconModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
