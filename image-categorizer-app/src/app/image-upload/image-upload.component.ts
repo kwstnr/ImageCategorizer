a1df5d1ef9922bfb7bc86156a01be83a85252d3a
Kevin Wüstner <kevin.wuestner@xappido.com>
2024-09-06 14:25:41 +0200

    feat: add image upload component

---
+++ image-categorizer-app/src/app/image-upload/image-upload.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-image-upload',
+  standalone: true,
+  imports: [],
+  templateUrl: './image-upload.component.html',
+  styleUrl: './image-upload.component.scss'
+})
+export class ImageUploadComponent {
+
+}
