d5ff412f9da12d483fe72b4f9ec1a2553e28f103
Kevin Wüstner <kevin.wuestner@xappido.com>
2024-09-06 07:41:27 +0200

    feat(auth): remove type restrictions from amplify configure

--- image-categorizer-app/src/aws-exports.ts
+++ image-categorizer-app/src/aws-exports.ts
@@ -1,11 +1,9 @@
-import { ResourcesConfig } from 'aws-amplify';
-
-const awsConfig: ResourcesConfig = {
+const awsConfig = {
   Auth: {
-    Cognito: {
-      userPoolId: 'us-east-1_8ErpaXsQ2',
-      userPoolClientId: 'm6h1tg0do4jraifc945nfuihe',
-    },
+    region: 'us-east-1',
+    userPoolId: 'us-east-1_8ErpaXsQ2',
+    userPoolWebClientId: 'm6h1tg0do4jraifc945nfuihe',
+    mandatorySignIn: true,
   },
 };
 
