751e5b9d7f35a16d9981f3eee637f09da25297a6
Kevin Wüstner <kevin.wuestner@xappido.com>
2024-09-09 19:11:49 +0200

    refactor(statistics): use observables and async pipe

--- image-categorizer-app/src/app/statistics/statistics.component.ts
+++ image-categorizer-app/src/app/statistics/statistics.component.ts
@@ -1,13 +1,12 @@
-import { Component, OnInit, inject } from '@angular/core';
-import { StatisticsService } from '../services/statistics/statistics.service';
-import { StatisticsResponse } from '../model/statistics-response.model';
-import { BehaviorSubject } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
-import { MatDialog } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { AsyncPipe } from '@angular/common';
+import { from, Observable } from 'rxjs';
+import { StatisticsResponse } from '../model/statistics-response.model';
+import { StatisticsService } from '../services/statistics/statistics.service';
 
 @Component({
   selector: 'app-statistics',
@@ -16,31 +15,16 @@ import { AsyncPipe } from '@angular/common';
     MatCardModule,
     MatButtonModule,
     MatIconModule,
-    MatProgressSpinnerModule,
     AsyncPipe,
-    MatProgressSpinnerModule
+    MatProgressSpinnerModule,
   ],
   templateUrl: './statistics.component.html',
   styleUrl: './statistics.component.scss',
 })
-export class StatisticsComponent implements OnInit {
+export class StatisticsComponent {
   private _statisticsService = inject(StatisticsService);
-  private readonly _dialog = inject(MatDialog);
 
-  private readonly _isLoadingSubject = new BehaviorSubject<boolean>(false);
-  readonly isLoading$ = this._isLoadingSubject.asObservable();
-
-  statistics: StatisticsResponse | null = null;
-  error: string | null = null;
-
-  ngOnInit(): void {
-    this.loadStatistics();
-  }
-
-  async loadStatistics() {
-    this._isLoadingSubject.next(true);
-    const response = await this._statisticsService.getStatistics();
-    this._isLoadingSubject.next(false);
-    this.statistics = response;
-  }
+  statistics$: Observable<StatisticsResponse | null> = from(
+    this._statisticsService.getStatistics()
+  );
 }
