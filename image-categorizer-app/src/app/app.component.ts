8426555e0fd45fd448c6537d1ddd317295f46013
Kevin Wüstner <kevin.wuestner@xappido.com>
2024-09-09 19:18:53 +0200

    fix: routing

--- image-categorizer-app/src/app/app.component.ts
+++ image-categorizer-app/src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 import { AmplifyAuthenticatorModule } from '@aws-amplify/ui-angular';
 
 @Component({
@@ -23,5 +23,10 @@ import { AmplifyAuthenticatorModule } from '@aws-amplify/ui-angular';
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'image-categorizer-app';
+  private _router = inject(Router);
+
+  navigate(route: string): void {
+    console.log('Navigating to:', route);
+    this._router.navigate([route]);
+  }
 }
